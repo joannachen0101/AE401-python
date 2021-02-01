df35cac898d757686e6b2e5c151062410757bbc2
joannachen0101 <77969826+joannachen0101@users.noreply.github.com>
2021-02-01 19:02:25 +0800

    Add files via upload

---
+++ 20210201 homework.py
@@ -0,0 +1,8 @@
+m=float(input('What is your math score?'))
+e=float(input('What is your english score?'))
+if m>=90 and e>=90:
+    print('有獎品')
+elif m>=90 or e>=90:
+    print('要加油')
+else:
+    print('要處罰')
