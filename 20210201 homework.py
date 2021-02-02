4de1b3d92f1f97df46231bbd96a712c47330ef5a
joannachen0101 <77969826+joannachen0101@users.noreply.github.com>
2021-02-02 09:12:37 +0800

    Update 20210201 homework.py

--- 20210201 homework.py
+++ 20210201 homework.py
@@ -2,7 +2,10 @@ m=float(input('What is your math score?'))
 e=float(input('What is your english score?'))
 if m>=90 and e>=90:
     print('有獎品')
-elif m>=90 or e>=90:
+elif m<60 and e<60:
+    print('要處罰')
+elif m<60 or e<60:
     print('要加油')
 else:
-    print('要處罰')
+    print('')
+
