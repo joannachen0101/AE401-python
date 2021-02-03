ae36fcf0443d476cb731c8e29022eb15909bee63
joannachen0101 <77969826+joannachen0101@users.noreply.github.com>
2021-02-03 17:04:53 +0800

    Add files via upload

---
+++ 20210203-3.py
@@ -0,0 +1,17 @@
+math=list()
+total=0
+avg=-0
+n=int(input('How many people in class?'))
+for i in range(n):
+    name=input('Please input the name:')
+    score=int(input('Please input.the.score:'))
+    age=int(input('Please input the age:'))
+    oneperson=list()
+    onepersom.append(name)
+    oneperson.append(score)
+    oneperson.append(age)
+    math.append(oneperson)
+    com_score.append(score)
+max_num=com_scorw.index(max(com_score))
+min_num=com_score.index(min(com_score))
+print('最高分為',max(com_score),'是',math[max_num][0],'最低分')     
